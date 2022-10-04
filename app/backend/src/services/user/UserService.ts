bb525a0a7c988e40679c82a3f3466b7489c7a6da
cacau <anaclaudia.sza@outlook.com>
2022-10-04 20:57:28 -0300

    feat: service login

---
+++ app/backend/src/services/user/UserService.ts
@@ -0,0 +1,24 @@
+import UserModel from '../../model/user/UserModel';
+import { ILogin } from '../../interfaces/ILogin';
+// import UserDTO from '../../interfaces/UserDTO';
+import TokenConfig from '../../utils/TokenConfig';
+import Bcrypt from '../../utils/Bcrypt';
+
+export default class UserService {
+  constructor(private model = new UserModel()) {}
+
+  login = async (user: ILogin) => {
+    // const findUser = await User.findOne({ where: { email: user } });
+    const findUser = await this.model.findOne(user.email);
+
+    if (!findUser) return { code: 401, message: 'Incorrect email or password' };
+
+    const passEncript = Bcrypt.compare(findUser.password, user.password);
+
+    if (!passEncript) return { code: 401, message: 'Incorrect email or password' };
+
+    const token = TokenConfig.createToken(user.email);
+
+    return { code: 200, token };
+  };
+}
