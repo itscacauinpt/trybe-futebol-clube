46d464806c51fa527dc2b3a5b4574e8020565e55
cacau <anaclaudia.sza@outlook.com>
2022-10-09 18:26:45 -0300

    feat: testes de erros, endpoint POST/login

--- app/backend/src/tests/mocks/userMock.ts
+++ app/backend/src/tests/mocks/userMock.ts
@@ -1,13 +1,4 @@
-// import UserDTO from "../../interfaces/UserDTO";
-
 import { IUser } from "../../interfaces/IUser";
-
-// const userMock: UserDTO = {
-//   username: 'Um Admin Legau',
-//   role: 'admin',
-//   email: 'admin',
-//   password: '$2a$08$xi.Hxk1czAO0nZR..B393u10aED0RQ1N3PAEXQ7HxtLjKPEZBu.PW',
-// }
 
 export const adm: IUser = {
   id: 1,
@@ -21,10 +12,6 @@ export const login = {
   admin: {
     email: 'admin@admin.com',
     password: 'secret_admin'
-  },
-  errorUser: {
-    email: 'nope@email.com',
-    password: '1234',
   },
   errorEmail:{
     email: 'email.email.com',
--- app/backend/src/tests/user.login.test.ts
+++ app/backend/src/tests/user.login.test.ts
@@ -4,11 +4,8 @@ import * as chai from 'chai';
 import chaiHttp = require('chai-http');
 import { app } from '../app';
 
-// import User from '../database/models/User';
 import UserModel from '../model/user/UserModel';
 import { login, adm } from './mocks/userMock';
-
-// import { Response } from 'superagent';
 
 chai.use(chaiHttp);
 
@@ -19,12 +16,31 @@ describe('Testing the Login Endpoint', () => {
   before(async () => sinon.stub(UserModel.prototype, 'findOne').resolves(adm));
   after(() => (UserModel.prototype.findOne as sinon.SinonStub).restore());
 
-  it('Endpoint POST, login com sucesso', async () => {
+  it('Endpoint POST succeeded', async () => {
     const post = await chai.request(app).post('/login').send(login.admin);
 
-    console.log(post.status)
-
     expect(post.status).to.equal(200);
-    // expect(post.body).to.deep.equal(adm);
+    expect(post.body).to.have.property('token');
+  });
+
+  it('Endpoint POST failed, requests not filled', async () => {
+    const post = await chai.request(app).post('/login').send();
+
+    expect(post.status).to.equal(400);
+    expect(post.body).to.deep.equal({ message: 'All fields must be filled' });
+  });
+
+  it('Endpoint POST failed, invalid email', async () => {
+    const post = await chai.request(app).post('/login').send(login.errorEmail);
+
+    expect(post.status).to.equal(400);
+    expect(post.body).to.deep.equal({message: 'Incorrect email or password'});
+  });
+
+  it('Endpoint POST failed, invalid password', async () => {
+    const post = await chai.request(app).post('/login').send(login.errorPassword);
+
+    expect(post.status).to.equal(400);
+    expect(post.body).to.deep.equal({message: 'Incorrect email or password'});
   });
 });
