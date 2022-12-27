b7a30badb4a33c49b03f6a2a48273df38edaaa9f
cacau <anaclaudia.sza@outlook.com>
2022-12-27 17:29:52 -0300

    feat: leaderboard

--- app/frontend/src/App.tsx
+++ app/frontend/src/App.tsx
@@ -1,8 +1,7 @@
-import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 
 // import MatchSettings from './pages/MatchSettings';
-// import Leaderboard from './pages/Leaderboard';
+import Leaderboard from './pages/Leaderboard';
 import Games from './pages/Games';
 import Login from './pages/Login';
 
@@ -12,7 +11,7 @@ function App() {
   return (
     <Routes>
       {/* <Route path="matches/settings" element={ <MatchSettings /> } /> */}
-      {/* <Route path="/leaderboard" element={ <Leaderboard /> } /> */}
+      <Route path="/leaderboard" element={ <Leaderboard /> } />
       <Route path="/matches" element={ <Games /> } />
       <Route path="/login" element={ <Login /> } />
       <Route path="/" element={ <Navigate to="/leaderboard" /> } />
